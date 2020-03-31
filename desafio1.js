62958368663497c87846136233fc8c440874f72e
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-03-31 16:24:58 -0300

    desafio 1 #goGirl

---
+++ desafio1.js
@@ -0,0 +1,10 @@
+/*
+  Inclua
+  o campo criadoPor
+  em todos os documentos,
+  colocando seu nome no valor desse campo.
+*/
+
+/*
+  db.produtos.updateMany( { }, { $set: { 'criadoPor': 'Bolivar' } } );
+*/
