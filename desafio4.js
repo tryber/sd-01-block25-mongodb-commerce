fd31d46a8964dcfab16dd3a40209690cab176ab4
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-03-31 17:10:25 -0300

    desafio 4, 5, 6

---
+++ desafio4.js
@@ -0,0 +1,11 @@
+/*
+  Incremente
+  o valor do campo `avaliacao` em `5`
+  em todos os sanduíches de carne do tipo `bovino`.
+  Dica: utilize como filtro o campo `tags`.
+*/
+
+/*
+  use commerce;
+  db.produtos.updateMany({ 'tags': 'bovino' }, { $inc: { 'avaliacao': 5 } });
+*/
---
+++ desafio5.js
@@ -0,0 +1,10 @@
+/*
+  Incremente
+  o valor do campo `avaliacao` em `3`
+  em todos os sanduíches de `ave`.
+*/
+
+/*
+use commerce;
+db.produtos.updateMany({ 'tags': 'ave' }, { $inc: { 'avaliacao': 3 } });
+*/
