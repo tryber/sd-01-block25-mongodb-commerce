b4b832eb51c74f9fe2e2f1e1f76b07d91fc871de
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-04-01 10:11:48 -0300

    desafio 12

---
+++ desafio10.js
@@ -0,0 +1,9 @@
+/*
+  Remova o primeiro `ingrediente`
+  do sanduíche `Quarteirão com Queijo`.
+*/
+
+/*
+use commerce;
+db.produtos.updateOne({ 'nome': 'Quarteirão com Queijo' }, { $pop: { 'ingredientes': -1 } });
+*/
---
+++ desafio12.js
@@ -0,0 +1,23 @@
+/*
+Agora, vamos simular a quantidade de vendas dos sanduíches
+por dia da semana.
+
+Para isso, inclua um _array_ com sete posições
+em todos os sanduíches.
+Cada uma delas representará um dia da semana, e cada posição iniciará em 0.
+O _array_ deve se parecer como abaixo:
+
+```json
+"vendasPorDia": [0, 0, 0, 0, 0, 0, 0]
+```
+
+O primeiro item desse _array_ representa as vendas no Domingo,
+enquanto o último representa as vendas no Sábado.
+Tenha isso claro porque vamos utilizar essa sequência mais à frente.
+
+*/
+
+/*
+use commerce;
+db.produtos.updateMany({}, { $set: { 'vendasPorDia': [0, 0, 0, 0, 0, 0, 0] } });s
+*/
